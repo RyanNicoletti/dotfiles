374fb08de6986095135584a00b3617cbdfc91f1d
Ryan Nicoletti <r.nicoletti1@gmail.com>
2024-04-08 21:15:28 -0400

    add keymaps for tabs

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set('n', '<leader>-', ':split<CR>')
 
 -- file explorer
 vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
+-- tabs
+vim.keymap.set('n', '<leader><tab>n', ':tabnext<CR>', {silent = true, nowait = true})
+vim.keymap.set('n', '<leader><tab>p', ':tabprevious<CR>', {silent = true, nowait = true})
+vim.keymap.set('n', '<leader><tab><tab>', ':tabnew<CR>', {silent = true, nowait = true})
+vim.keymap.set('n', '<leader><tab>d', ':tabclose<CR>', {silent = true, nowait = true})
