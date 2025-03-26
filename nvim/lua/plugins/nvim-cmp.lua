a22cc79a17ce83e92f25ee1fa939f15dc0fbb511
Ryan Nicoletti <r.nicoletti1@gmail.com>
2025-03-26 17:12:14 -0400

    add nvim and starship

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,43 @@
+return {
+	"hrsh7th/nvim-cmp",
+	enabled = true,
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			mapping = {
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
+}
